6473c3d2b7d197f7d289b4ab8f9cf9ef97610d43
tomoki10 <tomoki10wakhok@gmail.com>
2024-12-04 18:17:46 +0900

    Initial commit

---
+++ test/dsql-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DsqlLambda from '../lib/dsql-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dsql-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DsqlLambda.DsqlLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
